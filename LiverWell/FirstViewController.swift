48e4530927654522ca4e9926a3492186d9b0d5f1
Jo Hsu <jaune.yh@gmail.com>
2019-04-01 15:00:50 +0800

    Populate tabBar icon

---
+++ LiverWell/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  LiverWell
+//
+//  Created by 徐若芸 on 2019/4/1.
+//  Copyright © 2019 Jo Hsu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LiverWell/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LiverWell
+//
+//  Created by 徐若芸 on 2019/4/1.
+//  Copyright © 2019 Jo Hsu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
