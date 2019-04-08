34e795c13a1f6bd3363eb4c70eb230ab00ec3059
Jo Hsu <jaune.yh@gmail.com>
2019-04-08 20:38:53 +0800

    Initial PieChart cell setup in Status View Controller

--- LiverWell/Controller/Profile/StatusViewController.swift
+++ LiverWell/Controller/Profile/StatusViewController.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 
-class StatusViewController: UIViewController {
+class StatusViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
 
 
 
+}
+
+extension StatusViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PieChartTableViewCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+    
+    
 }
--- LiverWell/View/Profile/PieChartTableViewCell.swift
+++ LiverWell/View/Profile/PieChartTableViewCell.swift
@@ -7,18 +7,49 @@
 //
 
 import UIKit
+import Charts
 
 class PieChartTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    var trainDataEntry = PieChartDataEntry(value: 0)
+    
+    var stretchDataEntry = PieChartDataEntry(value: 0)
+
+    var numberOfDataEntries = [PieChartDataEntry]()
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        pieChartView.chartDescription?.text = ""
+        
+        trainDataEntry.value = 5
+        
+        stretchDataEntry.value = 10
+        
+        numberOfDataEntries = [trainDataEntry, stretchDataEntry]
+        
+        updateChartData()
+        
+    }
+    
+    func updateChartData() {
+        
+        let chartDataSet = PieChartDataSet(values: numberOfDataEntries, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor.orange, UIColor.G1]
+        chartDataSet.colors = colors as! [NSUIColor]
+        
+        pieChartView.data = chartData
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        
     }
     
 }
