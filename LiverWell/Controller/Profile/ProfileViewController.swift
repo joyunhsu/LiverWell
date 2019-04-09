762c3aafa350390cf794499ae3b639cc9409ef91
Jo Hsu <jaune.yh@gmail.com>
2019-04-09 14:26:37 +0800

    Enable Profile page scrollView paging when swpied

--- LiverWell/Controller/Profile/ProfileViewController.swift
+++ LiverWell/Controller/Profile/ProfileViewController.swift
@@ -8,12 +8,82 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
+class ProfileViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var indicatorView: UIView!
+    @IBOutlet weak var indicatorLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var statusContainerView: UIView!
+    @IBOutlet weak var weightContainerView: UIView!
+    
+    @IBOutlet var orderBtns: [UIButton]!
+    
+    var containerViews: [UIView] {
+        
+        return [statusContainerView, weightContainerView]
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+    }
+    
+    @IBAction func changePagePressed(_ sender: UIButton) {
+        
+        for btn in orderBtns {
+            
+            btn.isSelected = false
+            
+        }
+        
+        sender.isSelected = true
+        
+        moveIndicatorView(toPage: sender.tag)
+        
+    }
+    
+    private func moveIndicatorView(toPage: Int) {
+        
+        let screenWidth  = UIScreen.main.bounds.width
+        
+        indicatorLeadingConstraint.constant = CGFloat(toPage) * screenWidth / 2
+        
+        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+            
+            self?.scrollView.contentOffset.x = CGFloat(toPage) * screenWidth
+            
+            self?.view.layoutIfNeeded()
+            
+        })
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let screenWidth  = UIScreen.main.bounds.width
+        
+        indicatorLeadingConstraint.constant = scrollView.contentOffset.x / 2
+        
+        let temp = Double(scrollView.contentOffset.x / screenWidth)
+        
+        let number = lround(temp)
+        
+        for btn in orderBtns {
+            
+            btn.isSelected = false
+            
+        }
+        
+        orderBtns[number].isSelected = true
+        
+        UIView.animate(withDuration: 0.1, animations: { [weak self] in
+            
+            self?.view.layoutIfNeeded()
+            
+        })
+        
     }
     
 
