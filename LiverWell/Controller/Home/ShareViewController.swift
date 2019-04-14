b3c2e6ae97b2b989df64a78899a66a67cde936e3
Jo Hsu <jaune.yh@gmail.com>
2019-04-14 11:30:29 +0800

    Enable btn pressed logic in share function in home page

--- LiverWell/Controller/Home/ShareViewController.swift
+++ LiverWell/Controller/Home/ShareViewController.swift
@@ -14,11 +14,34 @@ class ShareViewController: UIViewController {
     
     @IBOutlet weak var weeklyStatusBtn: UIButton!
     
+    @IBOutlet var statusBtns: [UIButton]!
+    
+    @IBAction func selectStatusBtnPressed(_ sender: UIButton) {
+        
+        for btn in statusBtns {
+            
+            btn.isSelected = false
+            
+        }
+        
+        sender.isSelected = true
+        
+        selectStatus(withTag: sender.tag)
+        
+    }
+    
+    func selectStatus(withTag: Int) {
+        
+    }
+    
     @IBAction func dismissBtnPressed(_ sender: UIButton) {
+        
         dismiss(animated: true)
+        
     }
     
     @IBAction func shareBtnPressed(_ sender: UIButton) {
+        
     }
 
     override func viewDidLoad() {
