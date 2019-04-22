db09b45f58eb422adca4d0afdfe7cc5e5bd75d74
Jo Hsu <jaune.yh@gmail.com>
2019-04-22 13:27:27 +0800

    Create SettingVC in Profile Storyboard

--- LiverWell/Controller/Profile/ProfileViewController.swift
+++ LiverWell/Controller/Profile/ProfileViewController.swift
@@ -28,6 +28,19 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
         super.viewDidLoad()
 
         scrollView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = false
     }
 
     @IBAction func changePagePressed(_ sender: UIButton) {
---
+++ LiverWell/Controller/Profile/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  LiverWell
+//
+//  Created by 徐若芸 on 2019/4/22.
+//  Copyright © 2019 Jo Hsu. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
